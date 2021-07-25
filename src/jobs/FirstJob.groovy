4981c2f5c10bcff4d3844cb6254bdc4e4e208b76
shengkai23444 <shengkai23444@gmail.com>
2021-07-25 23:01:03 +0800

    update

--- src/jobs/FirstJob.groovy
+++ src/jobs/FirstJob.groovy
@@ -1,13 +1,13 @@
 job('first.job') {
-    description("This is My First Job")
+    // description("This is My First Job")
     logRotator {
         numToKeep 20
     }
-    parameters {
-        stringParam('FOO_STRINGPARAM', '', 'string param example')
-        booleanParam('FOO_BOOLEANPARAM', false, 'boolean param example')
-        choiceParam('FOO_CHOICEPARAM', ['foo1', 'foo2','foo3'], 'choice param example')
-    }
+    // parameters {
+    //     stringParam('FOO_STRINGPARAM', '', 'string param example')
+    //     booleanParam('FOO_BOOLEANPARAM', false, 'boolean param example')
+    //     choiceParam('FOO_CHOICEPARAM', ['foo1', 'foo2','foo3'], 'choice param example')
+    // }
     steps {
         shell('echo Hello My First Job!')
         shell('WORKPATH=$PWD')
