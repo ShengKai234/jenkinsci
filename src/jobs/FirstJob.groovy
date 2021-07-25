1d4f9955ce6000284451a8392e3107952be3b7a5
shengkai23444 <shengkai23444@gmail.com>
2021-07-25 23:02:11 +0800

    update

--- src/jobs/FirstJob.groovy
+++ src/jobs/FirstJob.groovy
@@ -1,19 +1,31 @@
 job('first.job') {
-    // description("This is My First Job")
     logRotator {
         numToKeep 20
     }
-    // parameters {
-    //     stringParam('FOO_STRINGPARAM', '', 'string param example')
-    //     booleanParam('FOO_BOOLEANPARAM', false, 'boolean param example')
-    //     choiceParam('FOO_CHOICEPARAM', ['foo1', 'foo2','foo3'], 'choice param example')
-    // }
     steps {
-        shell('echo Hello My First Job!')
-        shell('WORKPATH=$PWD')
-        shell('echo $WORKPATH')
+        shell('echo Hello World!')
     }
     wrappers {
         //timestamps()
+    }
+}
+
+pipelineJob('test.job.pipeline'){
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+
+    environmentVariables
+    {
+        env("NAME","LUCAS")
+        env("NUM","5566")
+    }
+
+    definition{
+
+        cps{
+            script(readFileFromWorkspace('src/pipeline/TestJobs-pipeline.groovy'))
+        }
     }
 }
