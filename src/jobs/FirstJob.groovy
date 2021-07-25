3668fc02d9b058729684ae37b766d89e2ae7fd7a
shengkai23444 <shengkai23444@gmail.com>
2021-07-25 22:45:12 +0800

    My First Job as Code

---
+++ src/jobs/FirstJob.groovy
@@ -0,0 +1,19 @@
+job('First.Job') {
+    description("This is My First Job")
+    logRotator {
+        numToKeep 20
+    }
+    parameters {
+        stringParam('FOO_STRINGPARAM', '', 'string param example')
+        booleanParam('FOO_BOOLEANPARAM', false, 'boolean param example')
+        choiceParam('FOO_CHOICEPARAM', ['foo1', 'foo2','foo3'], 'choice param example')
+    }
+    steps {
+        shell('echo Hello My First Job!')
+        shell('WORKPATH=$PWD')
+        shell('echo $WORKPATH')
+    }
+    wrappers {
+        //timestamps()
+    }
+}
