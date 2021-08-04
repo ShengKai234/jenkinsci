12087955ec7d5a2cf56041e1f2b5b778bca84a15
kai <kai@kaideMacBook-Pro.local>
2021-08-05 04:48:50 +0800

    add Git Job

---
+++ src/jobs/GitJobs.groovy
@@ -0,0 +1,26 @@
+job('Git_Job') {
+    description("This is my first git job!")
+    logRotator {
+        numToKeep 20
+    }
+    scm {
+        git {
+            remote {
+                github('jenkinsci/job-dsl-plugin.git')
+                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+            }
+            branch('*/master')
+        }
+    }
+    steps {
+        shell('echo Hello World!')
+        sh '''
+            javac Hello.java
+            java Hello
+        '''
+        // shell('echo Hello...this is my first Jenkins Job : $(date)')
+    }
+    wrappers {
+        //timestamps()
+    }
+}
