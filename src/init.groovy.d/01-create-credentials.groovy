19d5dec90feb2aeb07a846e68f05664dcfb91833
shengkai23444 <shengkai23444@gmail.com>
2021-08-18 09:12:05 +0800

    update slave node for vm

--- src/init.groovy.d/01-create-credentials.groovy
+++ src/init.groovy.d/01-create-credentials.groovy
@@ -1,19 +1,19 @@
-// import com.cloudbees.plugins.credentials.impl.*;
-// import com.cloudbees.plugins.credentials.*;
-// import com.cloudbees.plugins.credentials.domains.*;
-// import org.jenkinsci.plugins.plaincredentials.*
-// import org.jenkinsci.plugins.plaincredentials.impl.*
-// import com.cloudbees.plugins.credentials.common.*
-// import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
-// import hudson.util.Secret
-//
-// Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(
-// CredentialsScope.GLOBAL, // Scope
-// "my_id", // id
-// "My description", // description
-// "centos", // username
-// "centos" // password
-// )
-//
-// SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
-//
+import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.plugins.credentials.*;
+import com.cloudbees.plugins.credentials.domains.*;
+import org.jenkinsci.plugins.plaincredentials.*
+import org.jenkinsci.plugins.plaincredentials.impl.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import hudson.util.Secret
+
+Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(
+CredentialsScope.GLOBAL, // Scope
+"my_ubuntu_id", // id
+"My description ubuntu id", // description
+"kai", // username
+"kaiboy" // password
+)
+
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+
