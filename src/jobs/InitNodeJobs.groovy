6f213d372cfba305ee67d1efd861e790059b7e74
shengkai23444 <shengkai23444@gmail.com>
2021-11-01 01:00:13 +0800

    update init node job

--- src/jobs/InitNodeJobs.groovy
+++ src/jobs/InitNodeJobs.groovy
@@ -15,21 +15,17 @@ job('git.node.job.example') {
     }
     steps {
         shell("""
-            echo apt-get update
+            echo "apt-get update"
             echo \$password | sudo apt-get update
 
-            echo Install curl ...
+            echo "Install curl ..."
             sudo apt-get install -y curl
 
-            echo \$password | 
-            echo \$password | 
-
-            echo Install Docker ...
+            echo "Install Docker ..."
             echo \$password | sudo -y curl -fsSL https://get.docker.com/ | sh
             echo \$password | sudo docker --version
-            # echo \$password | sudo usermod -aG docker <your_username>
 
-            echo Install Docker-compose ...
+            echo "Install Docker-compose ..."
             echo \$password | sudo -y curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
             echo \$password | sudo chmod +x /usr/local/bin/docker-compose
             echo \$password | docker-compose –version
