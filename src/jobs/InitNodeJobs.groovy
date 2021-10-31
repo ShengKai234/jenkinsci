6648417df7d1cabe6e9d002491c681c0c6b50ecc
shengkai23444 <shengkai23444@gmail.com>
2021-11-01 01:05:01 +0800

    update init node job

--- src/jobs/InitNodeJobs.groovy
+++ src/jobs/InitNodeJobs.groovy
@@ -14,17 +14,17 @@ job('init.node.job.example') {
         shell("""
             echo \$password | sudo apt-get update
 
-            echo "Install curl ..."
-            sudo apt-get install -y curl
+            // echo "Install curl ..."
+            // sudo apt-get install -y curl
 
-            echo "Install Docker ..."
-            echo \$password | sudo -y curl -fsSL https://get.docker.com/ | sh
-            echo \$password | sudo docker --version
+            // echo "Install Docker ..."
+            // echo \$password | sudo -y curl -fsSL https://get.docker.com/ | sh
+            // echo \$password | sudo docker --version
 
-            echo "Install Docker-compose ..."
-            echo \$password | sudo -y curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-            echo \$password | sudo chmod +x /usr/local/bin/docker-compose
-            echo \$password | docker-compose –version
+            // echo "Install Docker-compose ..."
+            // echo \$password | sudo -y curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            // echo \$password | sudo chmod +x /usr/local/bin/docker-compose
+            // echo \$password | docker-compose –version
         """)
     }
     
