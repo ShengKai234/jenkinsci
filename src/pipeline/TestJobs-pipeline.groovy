c0b902978f9762367b4ff96c01d7e2c9a9d7ddfd
shengkai23444 <shengkai23444@gmail.com>
2021-11-15 00:12:51 +0800

    add pipeine example

--- src/pipeline/TestJobs-pipeline.groovy
+++ src/pipeline/TestJobs-pipeline.groovy
@@ -3,29 +3,58 @@ environment{
 }
 
 pipeline {
-    agent any 
-
+    agent { label 'example' }
     stages {
-
-        stage("Git") {
+        stage("Git-Checkout") {
             steps {
-                git url:  'echo "https://github.com/lucasko/gradle-task-example.git"'
+                echo "Checking out from Git Repo";
             }
         }
 
         stage('Build') {
             steps{
-                sh 'echo "./gradlew build --profile --no-daemon -s"'
+                echo "Building the checked-out project!";
             }
         }
 
-        stage('Test') {
+        stage('Unit-Test') {
             steps{
-                sh 'echo "./gradlew  test"'
+                echo "Running JUnit Tests";
+            }
+        }
+        
+        stage('Quality-Gate') {
+            steps{
+                echo "SonarQube Quality Gate passed successfully!!";
+                /*sh exit ("1");*/
+            }
+        }
+        
+        stage('Deploy') {
+            steps{
+                echo "Pass!";
             }
         }
 
     }//end of stages
+    post {
+        always {
+            echo 'This is always run'
+        }
+        success {
+            echo 'This will run only if successful'
+        }
+        failure {
+            echo 'This will run only if failed'
+        }
+        unstable {
+            echo 'This will run only if the run was marked as unstable'
+        }
+        changed {
+            echo 'This will run only if the state of the Pipline has changed'
+            echo 'For example, if the Pipline was previously failing but is now successful'
+        }
+    }
 }
 
 
